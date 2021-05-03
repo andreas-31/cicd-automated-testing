50f9af050a3abdb219cd3840c116f22d31757c06
Andreas Gaupmann <andreas@gaupmann.net>
2021-05-03 14:34:04 +0200

    Added vm module to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,3 +52,10 @@ module "publicip" {
   resource_type    = "publicip"
   resource_group   = "${module.resource_group.resource_group_name}"
 }
+module "vm" {
+  source           = "./modules/vm"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "vm"
+  resource_group   = "${module.resource_group.resource_group_name}"
+}
