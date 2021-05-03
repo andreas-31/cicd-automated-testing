65606bd70aa65ddb5a2da8b658498584af0cdb56
Andreas Gaupmann <andreas@gaupmann.net>
2021-05-03 14:30:23 +0200

    Initial data fill

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,23 +1,29 @@
-resource "azurerm_network_interface" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.application_type}-nic"
+  location            = "${azureLocation}"
+  resource_group_name = azurerm_resource_group.test.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = ""
+    subnet_id                     = azurerm_subnet.test.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = azurerm_public_ip.test.id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
-  size                = ""
-  admin_username      = ""
-  network_interface_ids = []
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "test" {
+    network_interface_id      = azurerm_network_interface.nic.id
+    network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.application_type}-vm"
+  location            = "${azureLocation}"
+  resource_group_name = azurerm_resource_group.test.name
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
   admin_ssh_key {
     username   = "azureuser"
     public_key = "file("~/.ssh/id_rsa.pub")"
