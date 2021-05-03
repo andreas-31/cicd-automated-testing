8c93cd44a51264955d1d4880e0b38d089ca4a4eb
Andreas Gaupmann <andreas@gaupmann.net>
2021-05-03 16:01:47 +0200

    Added nsg_id to vm module

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,4 +60,5 @@ module "vm" {
   resource_group   = "${module.resource_group.resource_group_name}"
   subnet_id        = "${module.network.subnet_id_test}"
   public_ip_address_id = "${module.publicip.public_ip_address_id}"
+  nsg_id           = "${module.nsg-test.nsg_id}"
 }
