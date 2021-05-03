e2cb3202ce6f47f403957c3a40b252f401c794e5
Andreas Gaupmann <andreas@gaupmann.net>
2021-05-03 15:38:12 +0200

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -5,9 +5,9 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = "${var.subnet_id_test}"
+    subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "${var.publicip.public_ip_address_id}"
+    public_ip_address_id          = "${var.public_ip_address_id}"
   }
 }
 
