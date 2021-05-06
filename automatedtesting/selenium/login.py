e5f9d6fa5e9d2e1f88596ce4ef544b99d8fa58c3
Ubuntu <azureuser@UbuntuTerminal.aj2333tiwxculhtlowp4co5gtg.bx.internal.cloudapp.net>
2021-05-06 08:50:46 +0000

    Added chrome-driver headless mode

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -7,12 +7,24 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 def login (user, password):
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    options = ChromeOptions()
+    options.binary_location = "/snap/bin/chromium"
+    options.add_argument("--headless")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-setuid-sandbox")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--disable-dev-shm-using")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+    #options.add_argument("start-maximized")
+    #options.add_argument("disable-infobars")
+    #options.add_argument(r"user-data-dir=.\cookies\\test")
+    chrome = webdriver.Chrome(options=options)
+    #driver = webdriver.Chrome()
     print ('Browser started successfully. Navigating to the demo page to login.')
-    driver.get('https://www.saucedemo.com/')
+    chrome.get('https://www.saucedemo.com/')
+    chrome.quit()
 
 login('standard_user', 'secret_sauce')
 
